4af0b016c3d377f3def5d9e2951f29a99246e950
angeloDarrow <aggelis.davelos@gmail.com>
2020-03-20 20:12:41 +0200

    closes #37.

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,27 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    private final RecipeService recipeService;
+
+    @Autowired
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"/", "", "/index"})
+        public String getHomePage(Model model) {
+
+        model.addAttribute("recipes",recipeService.getRecipes());
+
+            return "index";
+        }
+
+}
