fbf862f02837ae8fd3f2f7fb4d083416f9176298
angeloDarrow <aggelis.davelos@gmail.com>
2020-03-30 12:22:22 +0300

    Initial commit

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -18,9 +20,11 @@ public class IndexController {
 
     @RequestMapping({"/", "", "/index"})
         public String getHomePage(Model model) {
+        log.debug("Getting index page");
 
         model.addAttribute("recipes",recipeService.getRecipes());
 
+        log.debug("Hello from logger");
             return "index";
         }
 
